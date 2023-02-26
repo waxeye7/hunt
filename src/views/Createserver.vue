e166f13aaefb2d00465779bb3c09b0bc9c28820d
Merlin Stemmer <stemmermerlin@gmail.com>
2023-02-27 12:03:34 +1300

    Update Createserver.vue

--- src/views/Createserver.vue
+++ src/views/Createserver.vue
@@ -1,14 +1,22 @@
 <template>
     <div class="container center flex flex-col">
         <div class="flex flex-col center">
+            <h1 class="margin-bottom-much">your address: <span>{{ address }}</span></h1>
             <h1 class="margin-bottom-tiny">Waiting for Opponent to join...</h1>
             <img src="../assets/mr-bean-waiting.gif" alt="">
+            <div class="much-padding-bottom"></div>
         </div>
 
     </div>
 </template>
 
 <style scoped>
+.much-padding-bottom {
+    padding-bottom: 76px;
+}
+.margin-bottom-much {
+    margin-bottom: 80px;
+}
 .padding-top26percent {
     padding-top:26%;
 }
@@ -23,10 +31,17 @@
     justify-content: center;
     align-items: center;
 }
+span{
+    color:red;
+}
 </style>
 
 <script>
     export default {
-
+        data() {
+            return {
+                address:"192.158.1.38"
+            }
+        }
     }
 </script>
