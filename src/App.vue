195fbd80b747472d8a1f2a43443ec376f261f4f7
Merlin Stemmer <stemmermerlin@gmail.com>
2023-03-08 16:51:49 +1300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,10 @@
 <script setup>
-import Utku from "./components/Utku.vue";
 import Nav from "./components/Nav.vue";
-import MultiplayerBoard from "./components/MultiplayerBoard.vue";
 </script>
 <template>
 
-  <!-- <Utku /> -->
-
   <Nav />
   <router-view></router-view>
-  <!-- <MultiplayerBoard /> -->
 
 
 </template>
